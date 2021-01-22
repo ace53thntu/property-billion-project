b13e2910f0dbd3c7dff0215f0ccdba9919914798
Em Dinh <ace53thntu@gmail.com>
2021-01-22 18:09:29 +0700

    pgadmin, update readme

--- packages/server/src/env.d.ts
+++ packages/server/src/env.d.ts
@@ -2,5 +2,13 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     DATABASE_URL: string;
     PORT: string;
+    POSTGRES_HOST: string;
+    POSTGRES_PORT: string;
+    POSTGRES_USER: string;
+    POSTGRES_PASSWORD: string;
+    POSTGRES_DATABASE: string;
+    POSTGRES_DATABASE_TEST: string;
+    PGADMIN_DEFAULT_EMAIL: string;
+    PGADMIN_DEFAULT_PASSWORD: string;
   }
 }
